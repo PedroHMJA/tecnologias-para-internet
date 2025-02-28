98295fefb51f2c5d061db2974f918e5557deefb7
PedroHMJA <pedromayer.alves@outlook.com>
2025-02-27 22:25:45 -0300

    Adicionado dados da chamada à API na pagina web

--- api/api.js
+++ api/api.js
@@ -1,4 +1,19 @@
 async function buscaCep(cep){
     let resposta = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
-    return resposta.json();
+    let dados = await resposta.json()
+    document.getElementsByClassName('conteudo')[0].innerHTML = `
+        <h2>Endereço</h2>
+        <p>REGIÃO: ${dados.regiao}</p>
+        <p>ESTADO: ${dados.estado}</p>
+        <p>UF: ${dados.uf}</p>
+        <p>LOCALIDADE: ${dados.localidade}</p>
+        <p>BAIRRO: ${dados.bairro}</p>
+        <p>LOGRADOURO: ${dados.logradouro}</p>
+        <p>CEP: ${dados.cep}</p>
+    `;
+
+    
 }
+
+
+
